1a3e3363713576d923a8d0109aa8bc42aeab2a2a
ljj <3112125519@qq.com>
2023-02-14 14:18:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -119,38 +119,38 @@ data = {
     "value": get_week_day(),
     "color": get_random_color()
   },
-  "weather": {
-    "value": weather['weather'],
-    "color": get_random_color()
-  },
-  "humidity": {
-    "value": weather['humidity'],
-    "color": get_random_color()
-  },
-  "wind": {
-    "value": weather['wind'],
-    "color": get_random_color()
-  },
-  "air_data": {
-    "value": weather['airData'],
-    "color": get_random_color()
-  },
-  "air_quality": {
-    "value": weather['airQuality'],
-    "color": get_random_color()
-  },
-  "temperature": {
-    "value": math.floor(weather['temp']),
-    "color": get_random_color()
-  },
-  "highest": {
-    "value": math.floor(weather['high']),
-    "color": get_random_color()
-  },
-  "lowest": {
-    "value": math.floor(weather['low']),
-    "color": get_random_color()
-  },
+#   "weather": {
+#     "value": weather['weather'],
+#     "color": get_random_color()
+#   },
+#   "humidity": {
+#     "value": weather['humidity'],
+#     "color": get_random_color()
+#   },
+#   "wind": {
+#     "value": weather['wind'],
+#     "color": get_random_color()
+#   },
+#   "air_data": {
+#     "value": weather['airData'],
+#     "color": get_random_color()
+#   },
+#   "air_quality": {
+#     "value": weather['airQuality'],
+#     "color": get_random_color()
+#   },
+#   "temperature": {
+#     "value": math.floor(weather['temp']),
+#     "color": get_random_color()
+#   },
+#   "highest": {
+#     "value": math.floor(weather['high']),
+#     "color": get_random_color()
+#   },
+#   "lowest": {
+#     "value": math.floor(weather['low']),
+#     "color": get_random_color()
+#   },
   "love_days": {
     "value": get_memorial_days_count(),
     "color": get_random_color()
